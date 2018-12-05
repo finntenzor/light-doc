602cdac3982c798f3e118f96c0e13fb7c167c34e
finntenzor <finntenzor@gmail.com>
2018-12-05 19:45:55 +0800

    spliter支持触摸设备

--- src/components/spliter/index.vue
+++ src/components/spliter/index.vue
@@ -3,7 +3,9 @@
     <div class="ld-spliter-panel left" ref="left" :style="leftStyle">
       <slot name="left"></slot>
     </div>
-    <div class="ld-spliter-slider" @mousedown="handleDown"></div>
+    <div class="ld-spliter-slider" @mousedown="handleDown" @touchstart="handleDown">
+      <div class="ld-spliter-slider-icon"></div>
+    </div>
     <div class="ld-spliter-panel right" :style="rightStyle">
       <slot name="right"></slot>
     </div>
@@ -58,23 +60,41 @@ export default {
       const box = this.$refs.box
       return box ? box.clientWidth : 1
     },
-    handleMove(e) {
-      const offset = e.pageX - this.downX
+    handleOffset(offset) {
       const boxWidth = this.getBoxWidth()
       const rate = this.oldRate + offset / boxWidth
       this.rate = Math.max(0.15, Math.min(0.85, rate))
     },
+    handleMove(e) {
+      if (e instanceof MouseEvent) {
+        const offset = e.pageX - this.downX
+        this.handleOffset(offset)
+      } else if (e instanceof TouchEvent) {
+        const touch = e.changedTouches[0]
+        const offset = (touch ? touch.pageX : this.downX) - this.downX
+        this.handleOffset(offset)
+      }
+    },
     handleUp() {
+      off('touchmove', this.handleMove)
+      off('touchend', this.handleUp)
       off('mousemove', this.handleMove)
       off('mouseup', this.handleUp)
     },
     handleDown(e) {
       const leftWidth = this.$refs.left.clientWidth
       const boxWidth = this.getBoxWidth()
-      this.downX = e.pageX
       this.oldRate = leftWidth / boxWidth
-      on('mousemove', this.handleMove)
-      on('mouseup', this.handleUp)
+      if (e instanceof MouseEvent) {
+        this.downX = e.pageX
+        on('mousemove', this.handleMove)
+        on('mouseup', this.handleUp)
+      } else if (e instanceof TouchEvent) {
+        const touch = e.changedTouches[0]
+        this.downX = touch ? touch.pageX : this.downX
+        on('touchmove', this.handleMove)
+        on('touchend', this.handleUp)
+      }
     }
   }
 }
@@ -86,22 +106,34 @@ export default {
   overflow: hidden;
   width: 100%;
 }
+.ld-spliter-panel {
+  overflow-x: hidden;
+}
 .ld-spliter-panel.right {
   flex-grow: 1;
 }
 .ld-spliter-slider {
+  position: relative;
+  z-index: 9999;
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   flex: 0 0 20px;
   overflow-y: hidden;
   width: 20px;
   border-left: 1px solid #ebeef5;
   border-right: 1px solid #ebeef5;
   box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+  background-color: #fff;
+  cursor: col-resize;
+}
+.ld-spliter-slider-icon {
+  width: 100%;
+  height: 100%;
   background: url('~@/assets/spliter/icon.png');
   background-repeat: no-repeat;
   background-position: center center;
-  cursor: col-resize;
 }
 </style>
